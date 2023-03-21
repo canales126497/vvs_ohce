92ae70b724c7a4c254b79ef1524358eaabe492a4
David Canales Parra <canalesparradavid@gmail.com>
2023-03-21 11:29:11 +0000

    Test de reverse de textos e implementacion

--- src/Ohce.php
+++ src/Ohce.php
@@ -4,5 +4,15 @@ namespace Deg540\PHPTestingBoilerplate;
 
 class Ohce
 {
+    public function reverse(string $text_to_reverse): string
+    {
+        $reversed_text = '';
 
+        $text_legth = strlen($text_to_reverse);
+        for ($i = 0; $i < $text_legth; $i++) {
+            $reversed_text[$i] = $text_to_reverse[$text_legth - $i - 1];
+        }
+
+        return $reversed_text;
+    }
 }
--- tests/OhceTest.php
+++ tests/OhceTest.php
@@ -7,5 +7,15 @@ use PHPUnit\Framework\TestCase;
 
 class OhceTest extends TestCase
 {
+    /**
+     * @test
+     */
+    public function echosInReverse(): void
+    {
+        $ohce = new Ohce();
 
+        $ohceRessult = $ohce->reverse("hola");
+
+        $this->assertEquals("aloh", $ohceRessult);
+    }
 }
