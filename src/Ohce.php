67551816d6e5f8b50afdc953b6fcf10d0edf322b
David Canales Parra <canalesparradavid@gmail.com>
2023-03-21 11:42:32 +0000

    Rollback

--- src/Ohce.php
+++ src/Ohce.php
@@ -4,20 +4,4 @@ namespace Deg540\PHPTestingBoilerplate;
 
 class Ohce
 {
-    public function reverse(string $text_to_reverse): string
-    {
-        $reversed_text = '';
-
-        $text_legth = strlen($text_to_reverse);
-        for ($i = 0; $i < $text_legth; $i++) {
-            $reversed_text[$i] = $text_to_reverse[$text_legth - $i - 1];
-        }
-
-        return $reversed_text;
-    }
-
-    public function goodBye(): string
-    {
-        return "Adios Pedro";
-    }
 }
--- tests/OhceTest.php
+++ tests/OhceTest.php
@@ -7,27 +7,5 @@ use PHPUnit\Framework\TestCase;
 
 class OhceTest extends TestCase
 {
-    /**
-     * @test
-     */
-    public function echosInReverse(): void
-    {
-        $ohce = new Ohce();
 
-        $ohce_ressult = $ohce->reverse("hola");
-
-        $this->assertEquals("aloh", $ohce_ressult);
-    }
-
-    /**
-     * @test
-     */
-    public function saysGoodBye(): void
-    {
-        $ohce = new Ohce("Pedro");
-
-        $goodbye_text = $ohce->goodBye();
-
-        $this->assertEquals("Adios Pedro", $goodbye_text);
-    }
 }
