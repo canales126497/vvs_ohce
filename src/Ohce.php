296267203d82309b3be5bef42351a7a84e7dc0f0
David Canales Parra <canalesparradavid@gmail.com>
2023-03-21 15:40:02 +0000

    Creados test unitarios e implementaciones

--- src/Ohce.php
+++ src/Ohce.php
@@ -2,6 +2,50 @@
 
 namespace Deg540\PHPTestingBoilerplate;
 
+use Deg540\PHPTestingBoilerplate\DayMomentProvider;
+
 class Ohce
 {
+    private DayMomentProvider $day;
+
+    public function __construct(DayMomentProvider $day)
+    {
+        $this->day = $day;
+    }
+
+    public function execute(string $text_to_reverse): string
+    {
+        if (preg_match("/ohce/  ", $text_to_reverse) == 1) {
+            $day_moment = $this->day->getDayMoment();
+            $this->user_name = explode("ohce ", $text_to_reverse)[1];
+            if ($day_moment == DayMomentProvider::night) {
+                return "Buenas noches " . $this->user_name;
+            }
+            if ($day_moment == DayMomentProvider::morning) {
+                return "Buenos días " . $this->user_name;
+            }
+            if ($day_moment == DayMomentProvider::afternoon) {
+                return "Buenas tardes " . $this->user_name;
+            }
+
+            return "error en saludo";
+        }
+
+        if (preg_match("/Stop!/", $text_to_reverse) == 1) {
+            return "Adios " . $this->user_name;
+        }
+
+        $reversed_text = '';
+
+        $text_legth = strlen($text_to_reverse);
+        for ($i = 0; $i < $text_legth; $i++) {
+            $reversed_text[$i] = $text_to_reverse[$text_legth - $i - 1];
+        }
+
+        if (strcmp($text_to_reverse, $reversed_text) == 0) {
+            $reversed_text .= "\n¡Bonita palabra!";
+        }
+
+        return $reversed_text;
+    }
 }
--- tests/OhceTest.php
+++ tests/OhceTest.php
@@ -3,9 +3,91 @@
 namespace Deg540\PHPTestingBoilerplate\Test;
 
 use Deg540\PHPTestingBoilerplate\Ohce;
+use Deg540\PHPTestingBoilerplate\DayMomentProvider;
 use PHPUnit\Framework\TestCase;
+use Mockery;
 
 class OhceTest extends TestCase
 {
+    /**
+     * @test
+     */
+    public function canReverseText(): void
+    {
+        $ohce = new Ohce(new DayMomentProvider());
 
+        $ohce_result = $ohce->execute("hola");
+
+        $this->assertEquals("aloh", $ohce_result);
+    }
+
+    /**
+     * @test
+     */
+    public function canGreetMorningPerson(): void
+    {
+        $day_mock = new Mockery();
+        $day_mock = $day_mock->mock(DayMomentProvider::class);
+        $day_mock->allows()->getDayMoment()->andReturn(DayMomentProvider::morning);
+        $ohce = new Ohce($day_mock);
+
+        $ohce_result = $ohce->execute("ohce Pedro");
+
+        $this->assertEquals("Buenos días Pedro", $ohce_result);
+    }
+
+    /**
+     * @test
+     */
+    public function canGreetAfternoonPerson(): void
+    {
+        $day_mock = new Mockery();
+        $day_mock = $day_mock->mock(DayMomentProvider::class);
+        $day_mock->allows()->getDayMoment()->andReturn(DayMomentProvider::afternoon);
+        $ohce = new Ohce($day_mock);
+
+        $ohce_result = $ohce->execute("ohce Pedro");
+
+        $this->assertEquals("Buenas tardes Pedro", $ohce_result);
+    }
+
+    /**
+     * @test
+     */
+    public function canGreetNightPerson(): void
+    {
+        $day_mock = new Mockery();
+        $day_mock = $day_mock->mock(DayMomentProvider::class);
+        $day_mock->allows()->getDayMoment()->andReturn(DayMomentProvider::night);
+        $ohce = new Ohce($day_mock);
+
+        $ohce_result = $ohce->execute("ohce Pedro");
+
+        $this->assertEquals("Buenas noches Pedro", $ohce_result);
+    }
+
+    /**
+     * @test
+     */
+    public function canSayGoodbyeToPerson(): void
+    {
+        $ohce = new Ohce(new DayMomentProvider());
+
+        $ohce->execute("ohce Pedro");
+        $ohce_result = $ohce->execute("Stop!");
+
+        $this->assertEquals("Adios Pedro", $ohce_result);
+    }
+
+    /**
+     * @test
+     */
+    public function canDopalindromeTexts(): void
+    {
+        $ohce = new Ohce(new DayMomentProvider());
+
+        $ohce_result = $ohce->execute("oto");
+
+        $this->assertEquals("oto\n¡Bonita palabra!", $ohce_result);
+    }
 }
